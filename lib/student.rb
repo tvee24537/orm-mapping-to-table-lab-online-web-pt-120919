199b49bc7319b788f3da7b62f8904b84fe10d254
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-27 01:41:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,12 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  def initialize (name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  
   
   
 end
