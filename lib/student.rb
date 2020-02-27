6f5bb8c7ec89d7a71de0d9bd29c0dcbf5783940c
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-27 01:43:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,6 +23,13 @@ class Student
       DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)  
+  end
   
   
 end
