24d0af86b390f7687794f06c0a6b3beb6c2082e1
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-27 01:45:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,4 +32,15 @@ class Student
   end
   
   
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+        VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+  
+  
+  
 end
